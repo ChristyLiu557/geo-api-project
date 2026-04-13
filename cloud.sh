273b5f787d6131384fc8db3010de278580bd4b67
zl4505 <zl4505@nyu.edu>
2026-04-13 14:27:06 +0800

    Configure MongoDB Atlas cloud connection and fix connection settings

---
+++ cloud.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export FLASK_ENV=development
+export PROJ_DIR=$PWD
+export DEBUG=1
+export CLOUD_MONGO=1
+
+# run our server locally connected to cloud db:
+PYTHONPATH=$(pwd):$PYTHONPATH
+FLASK_APP=server.endpoints flask run --debug --host=127.0.0.1 --port=8000
